4650f150ab62980d309c7682e517325d43d2a92e
JoyZadan <joyzadan.learning@gmail.com>
2022-04-15 22:44:26 +0000

    Added docstrings for each function on run.py

--- run.py
+++ run.py
@@ -7,24 +7,65 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
+    """
+    The @app.route above is a decorator
+    A decorator is a way of wrapping functions.
+    The root decorator binds the index() function to itself, so that
+    whenever that root is called, the function is called.
+    This function renders content as HTML
+    Flask will look inside the templates directory or folder
+    to find the HTML files referenced in the return, ie, index page
+    """
     return render_template("index.html")
 
 
 @app.route("/about")
 def about():
+    """
+    This function renders content as HTML
+    Flask will look inside the templates directory or folder
+    to find the HTML files referenced in the return, ie about page.
+    Creates an empty directory to hold the json data.
+    """
     data = []
     with open("data/company.json", "r") as json_data:
         data = json.load(json_data)
     return render_template("about.html", page_title="About", company=data)
 
 
+@app.route("/about/<member_name>")
+def about_member(member_name):
+    """
+    Same as above functions. Additionally, creates an empty directory to hold
+    json data and for each object in data, creates separate url for each member
+    inside the json file and returns url in individual member's name
+    """
+    member = {}
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+        for obj in data:
+            if obj["url"] == member_name:
+                member = obj
+    return "<h1>" + member["name"] + "</h1>"
+
+
 @app.route("/contact")
 def contact():
+    """
+    Renders content as HTML
+    Flask will look inside of the templates directory or folder
+    to find the HTML files referenced in the return, ie contact
+    """
     return render_template("contact.html", page_title="Contact")
 
 
 @app.route("/careers")
 def careers():
+    """
+    Renders content as HTML
+    Flask will look inside of the templates directory or folder
+    to find the HTML files referenced in the return, ie careers
+    """
     return render_template("careers.html", page_title="Careers")
 
 
